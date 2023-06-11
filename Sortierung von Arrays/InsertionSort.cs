3082108adf213d67f1079d9e6610418a7e1a1faf
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2023-06-11 13:48:03 +0200

    Added InsertionSort

---
+++ Sortierung von Arrays/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortierung_von_Arrays
+{
+    internal class InsertionSort
+    {
+        public static int[] sort(int[] data)
+        {
+            for(int i = 0; i< data.Length; i++)
+            {
+                int minidx = getMinIndex(data, i);
+                (data[minidx], data[i]) = (data[i], data[minidx]);
+            }
+            return data;
+        }
+
+        static int getMinIndex(int[] data, int startidx=0)
+        {
+            int min = startidx;
+            for(int i = startidx+1; i< data.Length; i++)
+            {
+                if (data[min] > data[i])
+                    min = i;
+            }
+            return min;
+        }
+    }
+}
